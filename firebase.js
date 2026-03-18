3c62c520770aabd5e58f3a8fd8be596a09fa7b0a
yuucha5050-del <yuucha5050@gmail.com>
2026-03-18 12:28:23 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
+// Firebase設定（自分のに置き換え）
 const firebaseConfig = {
   apiKey: "AIzaSyBq5gZtI1opF5D8kljr_insUzRvbOA7HpA",
   authDomain: "chat-wave-a927f.firebaseapp.com",
-  projectId: "chat-wave-a927f",
+  projectId: "chat-wave-a927f"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
